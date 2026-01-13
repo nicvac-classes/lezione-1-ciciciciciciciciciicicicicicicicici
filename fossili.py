40e3a186aa6c8144b2c63df79c2b2940010a971b
Nicola Ruccia <nicolaruccia27@gmail.com>
2026-01-13 17:11:34 +0100

    Fossili

---
+++ fossili.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+# se preferisci leggere e scrivere da file
+# ti basta decommentare le seguenti due righe:
+
+sys.stdin = open('fossili_input_3.txt')
+sys.stdout = open('output.txt', 'w')
+
+
+def solve(t):
+    input()
+    a1, a2 = map(int, input().strip().split())
+    b1, b2 = map(int, input().strip().split())
+    c1, c2 = map(int, input().strip().split())
+
+    # aggiungi codice...
+   
+    risposta = min(a2, b2, c2) - max(a1, b1, c1)
+   
+
+    print(f"Case #{t}: {risposta}")
+
+
+T = int(input().strip())
+
+for t in range(1, T+1):
+    solve(t)
+
+sys.stdout.close()
